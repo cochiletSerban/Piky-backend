136b46a4c5f74a490f04f05de9539bcfaf59d97a
cochiletSerban <cochiletSernam@gmail.com>
2019-05-03 13:25:38 +0300

    switched /images endpoint to send json array of base 64 images to user

--- src/routers/image/imageController.js
+++ src/routers/image/imageController.js
@@ -14,18 +14,24 @@ let upload = async (req,  res) => {
 
 let getAll = async (req, res) => {
     try {
+        let base64Images = [];
         const user = req.user
         await user.populate('images').execPopulate()
-        const bufferImage = user.images[1].picture
-        res.send(bufferImage)
-        
-
+        user.images.forEach(image => {
+            base64Images.push('data:image/png;base64, ' + getBase64formImage(image.picture))
+        })
+        res.send(base64Images)
     } catch (e) {
         res.status(404).send()
     }
 }
 
+let getBase64formImage = function getBase64formImage (image) {
+    return image.toString('base64');
+}
+
 module.exports = {
     upload: upload, 
-    getAll: getAll
+    getAll: getAll,
+    getBase64formImage:getBase64formImage
 }
