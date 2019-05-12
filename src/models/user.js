5c66f93d4fbaa1aea9b6a8189061cf552ea855a2
cochiletSerban <cochiletSerban@gmail.com>
2019-05-12 22:52:30 +0300

    made username optional

--- src/models/user.js
+++ src/models/user.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        required: true,
+        required: false,
     },
     active: {
         type: Boolean,
