50d2c80c89f04fa6324e71b5f115d5a8b4d79767
cochiletSerban <cochiletSernam@gmail.com>
2019-06-04 13:58:23 +0300

    feat [IMAGES]: added getImageById route

--- src/routers/image/imageController.js
+++ src/routers/image/imageController.js
@@ -1,5 +1,8 @@
 const Image = require('../../models/image')
+const User = require('../../models/user')
 const ImageRating = require('../../models/imageRating')
+const jwt = require('jsonwebtoken')
+const httpErrors = require('http-errors')
 
 let upload = async (req,  res) => {
     let savedRating;
@@ -13,7 +16,8 @@ let upload = async (req,  res) => {
             description: req.body.description,
             picture: image.buffer,
             owner: req.user._id,
-            rating: savedRating._id
+            rating: savedRating._id,
+            private: req.body.private
         }).save()
 
 
@@ -23,7 +27,7 @@ let upload = async (req,  res) => {
 
 let getAllPublic = async (req, res) => {
     try {
-        const allImages = await Image.find({}) // add a query
+        const allImages = await Image.find({private: false}) // add a query
             .limit(parseInt(req.query.limit))
             .skip(parseInt(req.query.skip))
             .populate('rating').populate('owner').exec()
@@ -67,10 +71,52 @@ let resetImageRating = async(req, res) => {
         const ratingId = imageToLike.rating;
         await ImageRating.findOneAndUpdate({_id: ratingId}, {$pull:{likes:req.user._id}})
         await ImageRating.findOneAndUpdate({_id: ratingId}, {$pull:{dislikes:req.user._id}})
-
         res.status(200).send()
     } catch (e) {
         res.status(505).send()
+    }
+}
+
+let isUserValid = async(req) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')  
+        const decoded = jwt.verify(token, 'secret')
+        const user = await User.findOne({ _id: decoded._id })
+        
+        if (!user) {
+            throw new Error('ne-am dus dracu')
+        }
+        return user._id;
+    
+    } catch (e) {
+        return null;
+    }
+}
+
+let getImageById = async(req, res, next) => {
+    try {
+        
+        const image = await Image.findOne({_id: req.params.imageId}) // add a query
+            .populate('rating').populate('owner').exec()
+        
+        if (!image.private) {
+           return res.send(image)
+        } 
+
+        const userId = await isUserValid(req)
+
+        if(!userId) {
+            return next(httpErrors.BadRequest('esti un slaban si nu ai voie'))
+        }
+        
+        if (image.private && image.owner._id.toString() === userId.toString()) {
+            return res.send(image)
+        }
+
+        return next(httpErrors.BadRequest('esti un slaban si nu ai voie'))
+
+    } catch (e) {
+        res.status(404).json('n-am ce vrei')
     }
 }
 
@@ -79,5 +125,7 @@ module.exports = {
     getAllPublic: getAllPublic,
     likeImage: likeImage,
     dislikeImage: dislikeImage,
-    resetImageRating: resetImageRating
+    resetImageRating: resetImageRating,
+    getImageById: getImageById,
+    isUserValid:isUserValid
 }
--- src/routers/image/index.js
+++ src/routers/image/index.js
@@ -5,6 +5,7 @@ const multerUpload = require('./multerConfig').multerConfig
 
 imageRouter.post('', auth, multerUpload.array('image', 100), imageControler.upload)
 imageRouter.get('', imageControler.getAllPublic)
+imageRouter.get('/:imageId', imageControler.getImageById),
 imageRouter.post('/like', auth, imageControler.likeImage)
 imageRouter.post('/dislike', auth, imageControler.dislikeImage)
 imageRouter.post('/resetImageRating', auth, imageControler.resetImageRating)
