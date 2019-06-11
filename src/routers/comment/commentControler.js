c43942a38b863900ca0d120be5065353f87fae92
cochiletSerban <cochiletSernam@gmail.com>
2019-06-11 17:17:33 +0300

    feat [COMMENTCONTROLLER]: comments are sorted by most recent and post comment now retunrs the comment you posted

--- src/routers/comment/commentControler.js
+++ src/routers/comment/commentControler.js
@@ -8,9 +8,11 @@ let postComment = async(req, res) => {
             text: req.body.text
         }).save()
 
-       const img = await Image.findOneAndUpdate({_id: req.body.imageId}, {$push:{comms:comm._id}})
+       const image= await Image.findOneAndUpdate({_id: req.body.imageId}, {$push:{comms:comm._id}})
+       comms = await ImageComment.where('_id').in(image.comms)
+        .limit(parseInt(0)).sort({'createdAt': 'desc'}).populate('owner').exec()
 
-       res.status(201).json('gata boss nu mai comenta atata')
+       res.status(201).send(comms[0])
 
     } catch (e) {
         res.status(400).json('gura mica, ca nu ai voie')
@@ -22,7 +24,9 @@ let getCommentsByPictureId = async(req, res) => {
     comms = await ImageComment.where('_id').in(image.comms)
         .limit(parseInt(req.query.limit))
         .skip(parseInt(req.query.skip))
-        .populate('owner').exec()
+        .populate('owner')
+        .sort({'createdAt': 'desc'})
+        .exec()
     res.send(comms)
 }
 
