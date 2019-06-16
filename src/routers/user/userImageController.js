3d1c4e9cf0538d9afe1bdd466b8aa90b7d3caf89
cochiletSerban <cochiletSerban@gmail.com>
2019-06-16 17:06:01 +0300

    feat [LIKES][IMAGECONTROLER]: added posibility to sort by rating

--- src/routers/user/userImageController.js
+++ src/routers/user/userImageController.js
@@ -3,19 +3,36 @@ const Image = require('../../models/image')
 let getAll = async (req, res) => {
     try {
         const user = req.user
-        await user.populate({
-            path:'images',
-            options: {
-                limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip),
-                sort: {'createdAt': 'desc'}
-            },
-            match: {
-                private: req.query.private === undefined ? false : req.query.private
-            }
-        }).execPopulate()
-        res.send(user.images)
+        if (req.query.private !== undefined ) {
+            await user.populate({
+                path:'images',
+                options: {
+                    limit: parseInt(req.query.limit),
+                    skip: parseInt(req.query.skip),
+                    sort: {'createdAt': 'desc'}
+                },
+                match: {
+                    private: req.query.private,
+                    avatar: false
+                }
+            }).execPopulate()
+            res.send(user.images)
+        } else {
+            await user.populate({
+                path:'images',
+                match: {
+                    avatar: false
+                },
+                options: {
+                    limit: parseInt(req.query.limit),
+                    skip: parseInt(req.query.skip),
+                    sort: {'createdAt': 'desc'}
+                }
+            }).execPopulate()
+            res.send(user.images)
+        }
     } catch (e) {
+        console.log(e);
         res.status(404).send()
     }
 }
